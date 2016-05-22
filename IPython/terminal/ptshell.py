cc5a5be4a397161b7d6ba25e81fcd1d41adfc6ca
Thomas Kluyver <thomas@kluyver.me.uk>
2016-05-22 12:09:35 +0100

    Store command history for the debugger

--- IPython/terminal/ptshell.py
+++ IPython/terminal/ptshell.py
@@ -99,6 +99,7 @@ class TerminalInteractiveShell(InteractiveShell):
         self._update_layout()
 
     pt_cli = None
+    debugger_history = None
 
     autoedit_syntax = Bool(False,
         help="auto editing of files with syntax errors.",
@@ -350,6 +351,8 @@ class TerminalInteractiveShell(InteractiveShell):
         self.init_prompt_toolkit_cli()
         self.init_term_title()
         self.keep_running = True
+
+        self.debugger_history = InMemoryHistory()
 
     def ask_exit(self):
         self.keep_running = False
